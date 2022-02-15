8affcbbbe70b4b3fddd45ad817dac0b33f4f2a99
Yamnasm <jrts220@gmail.com>
2022-02-15 23:38:34 +0000

    Update PlayWordle.py

--- PlayWordle.py
+++ PlayWordle.py
@@ -1,15 +1,17 @@
 from tkinter import Tk, Label
 import tkinter.font as tkFont
-
+import wordle
 
 class WordleGUI:
     def __init__(self, master):
         self.master = master
         self.master.title("Wordle")
         self.master.geometry("200x200")
-        self.master.bind("<Configure>", self.font_resize)
+        self.master.bind("<Key>", self.keypress)
         self.font = tkFont.Font(family="Helvetica")
         self.label = []
+        self.letter_num = 0
+        self.word_size = 5
 
         i = 0
         for r in range(6):
@@ -20,17 +22,33 @@ class WordleGUI:
         master.columnconfigure(tuple(range(5)), weight=1)
         master.rowconfigure(tuple(range(6)), weight=1)
 
-    def font_resize(self, event):
-        for lbl in self.label:
-            x = lbl.winfo_width()
-            y = lbl.winfo_height()
-            if x < y:
-                lbl.configure(font=("Arial", x-25))
-            else:
-                lbl.configure(font=("Arial", y-25))
-
     def trigger(self, event):
         print("something happened")
+    
+    def valid_key(self, keycode):
+        if keycode >= 65 and keycode <= 90:
+            return True
+        elif keycode == 13 or keycode == 8:
+            return True
+        return False
+
+    def keypress(self, event):
+        if self.letter_num == len(self.label):
+            return
+        if self.valid_key(event.keycode) == False:
+            return
+
+        if event.keycode == 13:
+            kp = "ENTER"
+        elif event.keycode == 8:
+            kp = "BACKSPACE"
+        else:
+            kp = event.char
+
+        self.label[self.letter_num]["text"] = kp
+        self.letter_num += 1
+        
+        print(f"Pressed {kp}: {event.keycode}.")
 
 def main():
     root = Tk()
